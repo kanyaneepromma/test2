b80ed0027447f08586a16cb324bed40a2ea852fa
Kanyanee promma <kanyanee.pro@truedigital.com>
2021-02-20 08:11:10 +0700

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Kanyanee Promma on 20/2/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
